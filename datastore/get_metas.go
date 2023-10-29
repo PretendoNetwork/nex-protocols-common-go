85ac1b29c1d26610522c33f25fd7eaa2bf474257
Jonathan Barrow <jonbarrow1998@gmail.com>
2023-10-29 11:03:57 -0400

    added GetMetas

---
+++ datastore/get_metas.go
@@ -0,0 +1,159 @@
+package datastore
+
+import (
+	"github.com/PretendoNetwork/nex-go"
+	common_globals "github.com/PretendoNetwork/nex-protocols-common-go/globals"
+	datastore "github.com/PretendoNetwork/nex-protocols-go/datastore"
+	datastore_types "github.com/PretendoNetwork/nex-protocols-go/datastore/types"
+)
+
+func getMetas(err error, client *nex.Client, callID uint32, dataIDs []uint64, param *datastore_types.DataStoreGetMetaParam) uint32 {
+	if commonDataStoreProtocol.getObjectInfoByDataIDHandler == nil {
+		common_globals.Logger.Warning("GetObjectInfoByDataID not defined")
+		return nex.Errors.Core.NotImplemented
+	}
+
+	if commonDataStoreProtocol.verifyObjectPermissionHandler == nil {
+		common_globals.Logger.Warning("VerifyObjectPermission not defined")
+		return nex.Errors.Core.NotImplemented
+	}
+
+	if err != nil {
+		common_globals.Logger.Error(err.Error())
+		return nex.Errors.DataStore.Unknown
+	}
+
+	// TODO - Verify if param.PersistenceTarget is respected? It wouldn't make sense here but who knows
+
+	pMetaInfo := make([]*datastore_types.DataStoreMetaInfo, 0, len(dataIDs))
+	pResults := make([]*nex.Result, 0, len(dataIDs))
+
+	// * param has an AccessPassword, but it goes unchecked here.
+	// * The password would need to be the same for every object
+	// * in the input array, which doesn't make any sense. Assuming
+	// * it's unused until proven otherwise
+
+	for i := 0; i < len(dataIDs); i++ {
+		objectInfo, errCode := commonDataStoreProtocol.getObjectInfoByDataIDHandler(dataIDs[i])
+
+		if errCode != 0 {
+			// TODO - Maybe this should be broken out into a util function in globals?
+			objectInfo = datastore_types.NewDataStoreMetaInfo()
+			objectInfo.DataID = 0
+			objectInfo.OwnerID = 0
+			objectInfo.Size = 0
+			objectInfo.Name = ""
+			objectInfo.DataType = 0
+			objectInfo.MetaBinary = []byte{}
+			objectInfo.Permission = datastore_types.NewDataStorePermission()
+			objectInfo.Permission.Permission = 0
+			objectInfo.Permission.RecipientIDs = []uint32{}
+			objectInfo.DelPermission = datastore_types.NewDataStorePermission()
+			objectInfo.DelPermission.Permission = 0
+			objectInfo.DelPermission.RecipientIDs = []uint32{}
+			objectInfo.CreatedTime = nex.NewDateTime(0)
+			objectInfo.UpdatedTime = nex.NewDateTime(0)
+			objectInfo.Period = 0
+			objectInfo.Status = 0
+			objectInfo.ReferredCnt = 0
+			objectInfo.ReferDataID = 0
+			objectInfo.Flag = 0
+			objectInfo.ReferredTime = nex.NewDateTime(0)
+			objectInfo.ExpireTime = nex.NewDateTime(0)
+			objectInfo.Tags = []string{}
+			objectInfo.Ratings = []*datastore_types.DataStoreRatingInfoWithSlot{}
+
+			pResults = append(pResults, nex.NewResultError(errCode))
+		} else {
+			errCode = commonDataStoreProtocol.verifyObjectPermissionHandler(objectInfo.OwnerID, client.PID(), objectInfo.Permission)
+			if errCode != 0 {
+				// TODO - Maybe this should be broken out into a util function in globals?
+				objectInfo = datastore_types.NewDataStoreMetaInfo()
+				objectInfo.DataID = 0
+				objectInfo.OwnerID = 0
+				objectInfo.Size = 0
+				objectInfo.Name = ""
+				objectInfo.DataType = 0
+				objectInfo.MetaBinary = []byte{}
+				objectInfo.Permission = datastore_types.NewDataStorePermission()
+				objectInfo.Permission.Permission = 0
+				objectInfo.Permission.RecipientIDs = []uint32{}
+				objectInfo.DelPermission = datastore_types.NewDataStorePermission()
+				objectInfo.DelPermission.Permission = 0
+				objectInfo.DelPermission.RecipientIDs = []uint32{}
+				objectInfo.CreatedTime = nex.NewDateTime(0)
+				objectInfo.UpdatedTime = nex.NewDateTime(0)
+				objectInfo.Period = 0
+				objectInfo.Status = 0
+				objectInfo.ReferredCnt = 0
+				objectInfo.ReferDataID = 0
+				objectInfo.Flag = 0
+				objectInfo.ReferredTime = nex.NewDateTime(0)
+				objectInfo.ExpireTime = nex.NewDateTime(0)
+				objectInfo.Tags = []string{}
+				objectInfo.Ratings = []*datastore_types.DataStoreRatingInfoWithSlot{}
+
+				pResults = append(pResults, nex.NewResultError(errCode))
+			} else {
+				pResults = append(pResults, nex.NewResultSuccess(nex.Errors.DataStore.Unknown))
+			}
+
+			// * This is kind of backwards.
+			// * The database pulls this data
+			// * by default, so it can be done
+			// * in a single query. So instead
+			// * of checking if a flag *IS*
+			// * set, and conditionally *ADDING*
+			// * the fields, we check if a flag
+			// * is *NOT* set and conditionally
+			// * *REMOVE* the field
+			if param.ResultOption&0x1 == 0 {
+				objectInfo.Tags = make([]string, 0)
+			}
+
+			if param.ResultOption&0x2 == 0 {
+				objectInfo.Ratings = make([]*datastore_types.DataStoreRatingInfoWithSlot, 0)
+			}
+
+			if param.ResultOption&0x4 == 0 {
+				objectInfo.MetaBinary = make([]byte, 0)
+			}
+		}
+
+		pMetaInfo = append(pMetaInfo, objectInfo)
+	}
+
+	rmcResponseStream := nex.NewStreamOut(commonDataStoreProtocol.server)
+
+	rmcResponseStream.WriteListStructure(pMetaInfo)
+	rmcResponseStream.WriteListResult(pResults)
+
+	rmcResponseBody := rmcResponseStream.Bytes()
+
+	rmcResponse := nex.NewRMCResponse(datastore.ProtocolID, callID)
+	rmcResponse.SetSuccess(datastore.MethodGetMetas, rmcResponseBody)
+
+	rmcResponseBytes := rmcResponse.Bytes()
+
+	var responsePacket nex.PacketInterface
+
+	if commonDataStoreProtocol.server.PRUDPVersion() == 0 {
+		responsePacket, _ = nex.NewPacketV0(client, nil)
+		responsePacket.SetVersion(0)
+	} else {
+		responsePacket, _ = nex.NewPacketV1(client, nil)
+		responsePacket.SetVersion(1)
+	}
+
+	responsePacket.SetSource(0xA1)
+	responsePacket.SetDestination(0xAF)
+	responsePacket.SetType(nex.DataPacket)
+	responsePacket.SetPayload(rmcResponseBytes)
+
+	responsePacket.AddFlag(nex.FlagNeedsAck)
+	responsePacket.AddFlag(nex.FlagReliable)
+
+	commonDataStoreProtocol.server.Send(responsePacket)
+
+	return 0
+}
