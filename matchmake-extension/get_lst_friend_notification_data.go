a02cb95abf03e8d27c712166c3aec585446f911a
Daniel López Guimaraes <danielectra@outlook.com>
2025-01-27 21:26:06 +0000

    feat(matchmake-extension): Implement NotificationData methods
    The NotificationData methods are used by games to send notifications to
    friends about user activity, among others. These notifications are
    created or updated using `UpdateNotificationData`, which the server will
    register and send to the connected friends (as seen on Mario Tennis Open).
    
    The lifetime of these notifications is the same as the connection of the
    user who sends them. That is, when a user sends a notification and then
    disconnects, the notifications will be discarded.
    
    All notifications sent over `UpdateNotificationData` are logged inside
    the `tracking.notification_data` table to prevent abuse. The type of
    these notifications is also constrained to a range of specific values
    reserved for game-specific purposes (from 101 to 108).

---
+++ matchmake-extension/get_lst_friend_notification_data.go
@@ -0,0 +1,60 @@
+package matchmake_extension
+
+import (
+	"github.com/PretendoNetwork/nex-go/v2"
+	"github.com/PretendoNetwork/nex-go/v2/types"
+	matchmake_extension "github.com/PretendoNetwork/nex-protocols-go/v2/matchmake-extension"
+	notifications_types "github.com/PretendoNetwork/nex-protocols-go/v2/notifications/types"
+	common_globals "github.com/PretendoNetwork/nex-protocols-common-go/v2/globals"
+	"github.com/PretendoNetwork/nex-protocols-common-go/v2/matchmake-extension/database"
+)
+
+func (commonProtocol *CommonProtocol) getlstFriendNotificationData(err error, packet nex.PacketInterface, callID uint32, lstTypes types.List[types.UInt32]) (*nex.RMCMessage, *nex.Error) {
+	if err != nil {
+		common_globals.Logger.Error(err.Error())
+		return nil, nex.NewError(nex.ResultCodes.Core.InvalidArgument, err.Error())
+	}
+
+	connection := packet.Sender().(*nex.PRUDPConnection)
+	endpoint := connection.Endpoint().(*nex.PRUDPEndPoint)
+
+	notificationTypes := make([]uint32, len(lstTypes))
+	for i, notificationType := range lstTypes {
+		// * This method can only receive notifications within the range 101-108, which are reserved for game-specific notifications
+		if notificationType < 101 || notificationType > 108 {
+			return nil, nex.NewError(nex.ResultCodes.Core.InvalidArgument, "change_error")
+		}
+
+		notificationTypes[i] = uint32(notificationType)
+	}
+
+	commonProtocol.manager.Mutex.RLock()
+
+	notificationDatas, nexError := database.GetNotificationDatas(commonProtocol.manager, connection.PID(), notificationTypes)
+	if nexError != nil {
+		commonProtocol.manager.Mutex.RUnlock()
+		return nil, nexError
+	}
+
+	commonProtocol.manager.Mutex.RUnlock()
+
+	dataList := types.NewList[notifications_types.NotificationEvent]()
+	dataList = notificationDatas
+
+	rmcResponseStream := nex.NewByteStreamOut(endpoint.LibraryVersions(), endpoint.ByteStreamSettings())
+
+	dataList.WriteTo(rmcResponseStream)
+
+	rmcResponseBody := rmcResponseStream.Bytes()
+
+	rmcResponse := nex.NewRMCSuccess(endpoint, rmcResponseBody)
+	rmcResponse.ProtocolID = matchmake_extension.ProtocolID
+	rmcResponse.MethodID = matchmake_extension.MethodGetlstFriendNotificationData
+	rmcResponse.CallID = callID
+
+	if commonProtocol.OnAfterUpdateNotificationData != nil {
+		go commonProtocol.OnAfterGetlstFriendNotificationData(packet, lstTypes)
+	}
+
+	return rmcResponse, nil
+}
