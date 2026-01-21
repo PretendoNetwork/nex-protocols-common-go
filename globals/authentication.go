e61694deea7a130a5631bf3156aca251f41611e4
Jonathan Barrow <jonbarrow1998@gmail.com>
2026-01-21 17:18:50 -0500

    chore: remove unused NEXToken struct

--- globals/authentication.go
+++ globals/authentication.go
@@ -103,13 +103,3 @@ func ValidatePNLoginData(pid types.PID, loginData types.DataHolder, gameServerID
 
 	return nil
 }
-
-// NEXToken is the Pretendo-specific token format
-type NEXToken struct {
-	SystemType  uint8
-	TokenType   uint8
-	UserPID     uint32
-	ExpireTime  uint64
-	TitleID     uint64
-	AccessLevel int8
-}
