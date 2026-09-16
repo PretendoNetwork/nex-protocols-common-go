d237d39b1b5bab987e1d3241dc658471774e597c
BDMCGaming <bkipps09@gmail.com>
2026-09-16 19:11:16 -0400

    fix(match-making): update variable name

--- match-making/database/record_join_message.go
+++ match-making/database/record_join_message.go
@@ -7,8 +7,8 @@ import (
 )
 
 // RecordJoinMessage records the join message from a participant into a gathering
-func RecordJoinMessage(manager *common_globals.MatchmakingManager, gatheringID uint32, PID types.PID, message string) *nex.Error {
-	_, err := manager.Database.Exec(`INSERT INTO matchmaking.join_messages (gathering_id, pid, message) VALUES ($1, $2, $3) ON CONFLICT (gathering_id, pid) DO UPDATE SET message = $3`, gatheringID, PID, message)
+func RecordJoinMessage(manager *common_globals.MatchmakingManager, gatheringID uint32, sourcePID types.PID, message string) *nex.Error {
+	_, err := manager.Database.Exec(`INSERT INTO matchmaking.join_messages (gathering_id, pid, message) VALUES ($1, $2, $3) ON CONFLICT (gathering_id, pid) DO UPDATE SET message = $3`, gatheringID, sourcePID, message)
 	if err != nil {
 		return nex.NewError(nex.ResultCodes.Core.SystemError, err.Error())
 	}
